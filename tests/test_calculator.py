b57b933840891fb29ba913c9ef8b7850b01d047e
Bianca <bzbaccay@my.waketech.edu>
2026-03-23 22:21:24 -0400

    feat: add calculator with CI

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,15 +8,15 @@ def test_add():
     assert add(2, 3) == 5
 
 
-def test_subtract():  
+def test_subtract():
     assert subtract(5, 3) == 2
 
 
-def test_multiply():  
+def test_multiply():
     assert multiply(3, 4) == 12
 
 
-def test_divide():    
+def test_divide():
     assert divide(10, 2) == 5.0
 
 
